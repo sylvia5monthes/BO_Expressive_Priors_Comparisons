3cfa6f5c1694ea5e701697209ab2b0baccba4f20
Sara <saramiskovich@gmail.com>
2021-07-30 15:17:04 -0400

    added nb with examples using request functions for the surrogate

--- injector_surrogate/emittance_calc.py
+++ injector_surrogate/emittance_calc.py
@@ -7,13 +7,13 @@ d = 2.2 # [m] distance between Q525 and OTR2
 l = 0.108 # effective length [m]
 
 def getGradient(b_field, l_eff=0.108):
-    ''' Calculates the quad field gradient [T/m] 
+    ''' Returns the quad field gradient [T/m] 
         l_eff: effective length [m] 
         b_field: integrated field [kG] '''
     return b_field *0.1 /l_eff
     
 def getK1(g, p):
-    '''Gets quad strength [1/m^2]
+    '''Returns quad strength [1/m^2]
        g: quad field gradient [T/m]
        p: momentum [GeV] (or alternatively beta*E [GeV])'''
     return 0.2998 * g / p
@@ -33,19 +33,19 @@ def fitSigma(sizes, k, d=d, l=l):
     return sig_11, sig_12, sig_22
 
 def getEmit(sig11, sig12, sig22):
-    '''Find emittance (not normalized)'''
+    '''Returns emittance (not normalized)'''
     
     try:
         with warnings.catch_warnings():
             warnings.simplefilter("error")
-    emit  = np.sqrt(sig11*sig22 - sig12**2)
-    
+            emit  = np.sqrt(sig11*sig22 - sig12**2)
+            return emit
     except RuntimeWarning:
-        return 1000 # arbitrary high value, probably not the best way to handle this
-    
-    return emit
+        return 1000 # arbitrary high value, maybe the best way to handle this?
 
 def getNormEmit(energy, quad_vals, xrms, yrms):
+    '''Returns normalized emittance [m]
+       given quad values and beamsizes'''
     gamma = energy/m_0
 
     b1_gradient = getGradient(quad_vals)
